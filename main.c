6d91f0d2b05ff97124123e21375b2cfa3404399a
Nathália Braz Krawczyk <111463766+nathaliabrazk@users.noreply.github.com>
2022-09-24 11:18:37 -0300

    inicio de cadastro

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	// Propriedades
+	char nome[50], email[50];
+	int  dia, mes, ano, endereco, cpf, tel;
+	
+	// Entradas
+	/*printf("Digite o nome: ");
+	scanf("%s", nome);
+	printf("Digite email: ");
+	scanf("%s", email);*/
+	printf("Digite o dia", dia);
+	scanf("%d", dia);
+	
+	
+	// Processamento
+	
+	
+	// Saida
+	/*printf("O nome digitado e: %s", nome);
+	printf("O email digitado e: %s", email);*/
+	scanf("O dia digitado foi:  %d", dia);
+	return 0;
+}
