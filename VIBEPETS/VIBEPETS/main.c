4f6dfdad0068a868dc5f6929a4eac9fc487f570a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-09-26 10:02:17 -0300

    initial commit

---
+++ VIBEPETS/VIBEPETS/main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  VIBEPETS
+//
+//  Created by Nathália Braz on 24/09/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // Propriedades
+    char nome[50], email[50];
+    int  dia, mes, ano, endereco, cpf, tel;
+    
+    // Entradas
+    /*printf("Digite o nome: ");
+    scanf("%s", nome);
+    printf("Digite email: ");
+    scanf("%s", email);*/
+    printf("Digite o dia: ");
+    scanf("%d", &dia);
+    
+    
+    // Processamento
+    
+    
+    // Saida
+    /*printf("O nome digitado e: %s", nome);
+    printf("O email digitado e: %s", email);*/
+    scanf("O dia digitado foi:  %d", &dia);
+    return 0;
+}
