0d40002ea749e5f7010c106ca19bbccebc776de9
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-10-01 22:37:38 -0300

    chore: adicionei escrita de arquivos a Main

--- VIBEPETS/main.c
+++ VIBEPETS/main.c
@@ -2,121 +2,138 @@
 //  main.c
 //  VIBEPETS
 //
-//  Created by NathÃ¡lia Braz on 24/09/22.
+//  Created by Nathália Braz on 24/09/22.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h> 
+#include <locale.h>
 
+// PROTOTIPOS
 
 int main(int argc, char *argv[]) {
- 	setlocale (LC_ALL, ""); 
+    setlocale (LC_ALL, "");
     // Propriedades
-    char nome[50], email[50], opcao;
-    int  dia, mes, ano, endereco, cpf, tel;
+    /*
+     char nome[50], email[50], opcao;
+     int  dia, mes, ano, endereco, cpf, tel;
+     
+     // Entradas
+     
+     printf("Digite o nome: ");
+     scanf("%s", nome);
+     printf("Digite email: ");
+     scanf("%s", email);
+     printf("Digite o dia: ");
+     scanf("%d", &dia);
+     
+     
+     // Processamento
+     
+     
+     // Saida
+     printf("O nome digitado e: %s", nome);
+     printf("O email digitado e: %s", email);
+     
+     scanf("O dia digitado foi:  %d", &dia);
+     
+     printf ("Escolha os produtos que deseja da linha para o dia de Spa do seu pet\n");
+     printf ("Escolha o Shampoo para seu pet:\n");
+     printf ("A- Shampoo Pré-Banho Pelôncio \nB- Shampoo Pelos Claros Pelôncio \nC- Shampoo Pelos Escuros Pelôncio \nFragrâncias:	Avelã - Melância - Dove - Morango\n");
+     printf ("Opção:\n");
+     scanf ("%s", &opcao);
+     fflush (stdin);
+     system ("cls");
+     
+     switch (opcao) {
+     case 'A':
+     case 'a':
+     printf ("Você escolheu  Shampoo Pré-Banho Pelôncio\n");
+     break;
+     case 'B':
+     case 'b':
+     printf ("Você escolheu Shampoo Pelos Claros Pelôncio\n");
+     break;
+     case 'C':
+     case 'c':
+     printf ("Você escolheu Shampoo Pelos Escuros Pelôncio\n");
+     break;
+     default:
+     printf ("Opção inválida");
+     }
+     
+     printf ("Escolha a fragrância do Condicionador para seu pet:\n");
+     printf ("A-Avelã \nB- Melância \nC- Morango\n");
+     printf ("Opção:\n");
+     scanf ("%c", &opcao);
+     fflush (stdin);
+     system ("cls");
+     
+     switch (opcao) {
+     case 'A':
+     case 'a':
+     printf ("Você escolheu Avelã\n");
+     break;
+     case 'B':
+     case 'b':
+     printf ("Você escolheu Melância\n");
+     break;
+     case 'C':
+     case 'c':
+     printf ("Você escolheu Morango\n");
+     break;
+     default:
+     
+     printf ("Opção inválida");
+     }
+     printf ("Escolha a colônia Perfume Pet Clean Max para seu pet:\n");
+     printf ("A-  Amêndoas e Ameixa \nB- Avelã e Cereja \nC- Melancia\n");
+     printf ("Opção:\n");
+     scanf ("%c", &opcao);
+     fflush (stdin);
+     system ("cls");
+     
+     switch (opcao)
+     {
+     case 'A':
+     case 'a':
+     printf ("Você escolheu Amêndoas e Ameixa\n");
+     break;
+     case 'B':
+     case 'b':
+     printf ("Você escolheu Avelã e Cereja\n");
+     break;
+     case 'C':
+     case 'c':
+     printf ("Você escolheu Melância\n");
+     break;
+     default:
+     printf ("Opção inválida");
+     }
+     */
+    // Criando a variável ponteiro para o arquivo
+    FILE *pont_arq;
+    char palavra[20]; // variável do tipo string
 
-    // Entradas
+    // Abrindo o arquivo
+    pont_arq = fopen("arquivo.txt", "a");
+    
+    // Mensagem para o usuário
+    if (pont_arq == NULL) {
+        printf("ERRO! O arquivo não foi aberto!\n\n\n");
+        
+    } else {
+        printf("O arquivo foi aberto com sucesso!\n\n\n");
+    }
+    
+    // Fechando arquivo
+    fclose(pont_arq);
+    
+    
+    system("pause");
+    
+    return 0;
+}
 
-    /*printf("Digite o nome: ");
-    scanf("%s", nome);
-    printf("Digite email: ");
-    scanf("%s", email);*/
-    printf("Digite o dia: ");
-    scanf("%d", &dia);
-    
-    
-    // Processamento
-    
-    
-    // Saida
-    /*printf("O nome digitado e: %s", nome);
-    printf("O email digitado e: %s", email);*/
-    scanf("O dia digitado foi:  %d", &dia);
-    
-	printf ("Escolha os produtos que deseja da linha para o dia de Spa do seu pet\n"); 
-	printf ("Escolha o Shampoo para seu pet:\n");
-	printf ("A- Shampoo Pré-Banho Pelôncio \nB- Shampoo Pelos Claros Pelôncio \nC- Shampoo Pelos Escuros Pelôncio \nFragrâncias:	Avelã - Melância - Dove - Morango\n"); 
-	printf ("Opção:\n"); 
-	scanf ("%s", &opcao); 
-	fflush (stdin); 
-	system ("cls"); 
-	  
-	switch (opcao) 
-	
-	{ 
-	case 'A':  
-	case 'a': 
-	printf ("Você escolheu  Shampoo Pré-Banho Pelôncio\n"); 
-	break; 
-	case 'B': 
-	case 'b': 
-	printf ("Você escolheu Shampoo Pelos Claros Pelôncio\n"); 
-	break;
-	case 'C':  
-	case 'c': 
-	printf ("Você escolheu Shampoo Pelos Escuros Pelôncio\n"); 
-	break; 
-	default: 
-	printf ("Opção inválida"); 
-	}
-	
-	printf ("Escolha a fragrância do Condicionador para seu pet:\n");
-	printf ("A-Avelã \nB- Melância \nC- Morango\n");
-	printf ("Opção:\n");
-	scanf ("%c", &opcao);
-	fflush (stdin); 
-	system ("cls"); 
-	
-	switch (opcao)
-	
-	{
-	case 'A':  
-	case 'a': 
-	printf ("Você escolheu Avelã\n"); 
-	break; 
-	case 'B':  
-	case 'b': 
-	printf ("Você escolheu Melância\n"); 
-	break; 
-	case 'C': 
-	case 'c': 
-	printf ("Você escolheu Morango\n"); 
-	break; 
-	default: 
-	
-	printf ("Opção inválida");  
-	}
-	printf ("Escolha a colônia Perfume Pet Clean Max para seu pet:\n"); 
-	printf ("A-  Amêndoas e Ameixa \nB- Avelã e Cereja \nC- Melancia\n"); 
-	printf ("Opção:\n"); 
-	scanf ("%c", &opcao); 
-	fflush (stdin); 
-	system ("cls"); 
-	
-	switch (opcao)
-	{
-	case 'A': 
-	case 'a': 
-	printf ("Você escolheu Amêndoas e Ameixa\n"); 
-	break; 
-	case 'B':  
-	case 'b': 
-	printf ("Você escolheu Avelã e Cereja\n"); 
-	break; 
-	case 'C': 
-	case 'c': 
-	printf ("Você escolheu Melância\n"); 
-	break; 
-	default: 
-	printf ("Opção inválida");
-	}
-	
-	
-	
-	
-	  
-	    
-	    return 0;
-	    
-	}
+//PERSISTENCIA EM XML
+
