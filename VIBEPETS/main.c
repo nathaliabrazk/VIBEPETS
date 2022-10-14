e37c967242fa4882db90fdd4f3226c9a11ecb6fd
alicettins <111758338+alicettins@users.noreply.github.com>
2022-10-13 22:55:55 -0300

    valor: combo
    adicionei valor para cada combo

--- VIBEPETS/main.c
+++ VIBEPETS/main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
     
 printf ("Escolha um dos nossos servicos!\n "); 
 	printf ("OBSERVAÇÃO (SÓ PODE SER ESCOLHIDO UM COMBO DE SERVIÇO POR DIA E POR PET)\n");
-	printf ("A)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Banho de Carinho Petz\n-Condicionador Chegou o Brincalhão Petz\n-Colônia Hydra Groomers Forevers\n-Creme Hidratante de Patinhas\n-Vela Aromática Perfumada | Lavanda\nB)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Pet Society Neutro\n-Condicionador Pet Society\n-Colônia Pet Society Lovely\n-Creme hidrantante Pet Society Lovely\n-Vela Aromática Perfumada| Canela\nC)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Matacura Sarnicida e Anti-Pulgas\n-Condicionador Matacura Sarnicida e Anti-Pulgas\n-Perfume Ibasa de Colônia Affection para Cães e Gatos\n-Creme Hidratante Equilíbrio Hidrapet\nVela Aromática Perfumada | Baunilha\n"); 
+	printf ("A)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Banho de Carinho Petz\n-Condicionador Chegou o Brincalhão Petz\n-Colônia Hydra Groomers Forevers\n-Creme Hidratante de Patinhas\n-Vela Aromática Perfumada | Lavanda\n|VALOR: 100,00|\nB)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Pet Society Neutro\n-Condicionador Pet Society\n-Colônia Pet Society Lovely\n-Creme hidrantante Pet Society Lovely\n-Vela Aromática Perfumada| Canela\n|VALOR: 90,00|\nC)------------------------------------------------------------------\n-Tosa e banho\n-Shampoo Matacura Sarnicida e Anti-Pulgas\n-Condicionador Matacura Sarnicida e Anti-Pulgas\n-Perfume Ibasa de Colônia Affection para Cães e Gatos\n-Creme Hidratante Equilíbrio Hidrapet\nVela Aromática Perfumada | Baunilha\n|VALOR: 80,00|\n"); 
 	printf ("OPCAO:\n"); 
 	scanf ("%s", &opcao); 
 	fflush (stdin); 
@@ -47,15 +47,15 @@ printf ("Escolha um dos nossos servicos!\n ");
 	{ 
 	case 'A':  
 	case 'a': 
-	printf ("Voce escolheu o combo (A)\n-Tosa e banho\n-Shampoo Banho de Carinho Petz\n-Condicionador Chegou o Brincalhão Petz\n-Colônia Hydra Groomers Forevers\n-Creme Hidratante de Patinhas\n-Vela Aromática Perfumada | Lavanda\n"); 
+	printf ("Voce escolheu o combo (A)\n-Tosa e banho\n-Shampoo Banho de Carinho Petz\n-Condicionador Chegou o Brincalhão Petz\n-Colônia Hydra Groomers Forevers\n-Creme Hidratante de Patinhas\n-Vela Aromática Perfumada | Lavanda\n|VALOR: 100,00|\n"); 
 	break; 
 	case 'B': 
 	case 'b': 
-	printf ("Voce escolheu o combo (B)\n-Tosa e banho\n-Shampoo Pet Society Neutro\n-Condicionador Pet Society\n-Colônia Pet Society Lovely\n-Creme hidrantante Pet Society Lovely\n-Vela Aromática Perfumada| Canela\n"); 
+	printf ("Voce escolheu o combo (B)\n-Tosa e banho\n-Shampoo Pet Society Neutro\n-Condicionador Pet Society\n-Colônia Pet Society Lovely\n-Creme hidrantante Pet Society Lovely\n-Vela Aromática Perfumada| Canela\n|VALOR: 90,00|\n"); 
 	break;
 	case 'C':  
 	case 'c': 
-	printf ("Voce escolheu o combo (C)\n-Tosa e banho\n-Shampoo Matacura Sarnicida e Anti-Pulgas\n-Condicionador Matacura Sarnicida e Anti-Pulgas\n-Perfume Ibasa de Colônia Affection para Cães e Gatos\n-Creme Hidratante Equilíbrio Hidrapet\nVela Aromática Perfumada | Baunilha\n"); 
+	printf ("Voce escolheu o combo (C)\n-Tosa e banho\n-Shampoo Matacura Sarnicida e Anti-Pulgas\n-Condicionador Matacura Sarnicida e Anti-Pulgas\n-Perfume Ibasa de Colônia Affection para Cães e Gatos\n-Creme Hidratante Equilíbrio Hidrapet\nVela Aromática Perfumada | Baunilha\n|VALOR: 80,00|\n"); 
 	break; 
 	default: 
 	printf ("OPCAO INVALIDA"); 
