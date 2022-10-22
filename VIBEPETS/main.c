7331eedc6384dd6e8068b4f04aa90716fbf99490
Mário Neto <73663266+marioprneto@users.noreply.github.com>
2022-10-22 11:04:15 -0300

    Correção do bug e alteração do método de us/senha

--- VIBEPETS/main.c
+++ VIBEPETS/main.c
@@ -18,30 +18,85 @@ int main(int argc, char *argv[]) {
     //Entradas do sistema de login
     setlocale(LC_ALL, "Portuguese");
 
-    char usuario[75], senha[50], c;
-    int i=10, j=0, v=0;
-    while (v==0){
-        printf("Digite o seu login: ");
-        gets(usuario);
-        printf("Digite sua senha: ");
-        while((c=getch())!=13){
-            senha[j] = c;
-            j++;
-            printf("*");
-        }
-        senha[j]='\0';
-        j=0;
-        system("cls");
-        i = strcmp(senha,"josafa");
-        if(i==0){
-            printf("Login autorizado! Seja bem vindo(a).");
-            v=1;
-        } else{
-            printf("Login não autorizado! Usuário ou senha estão incorretos!");
-            printf("\nTente novamente.\n");
-        }
+    #include<stdio.h>
+    #include<string.h>
+    #include<stdlib.h>
+    #include<conio.h>
+    #define backSpace 8
+    #define newLine 13
 
+    int GetPassword(char *str);
+
+    void main()
+    {
+        char senha[30], usuario[50];
+        int usuarioDigitado, senhaDigitada;
+        printf("Bem-vindo(a) ao VIBE Pets. Faça seu login para continuar.\n");
+        printf("Digite seu usuário: ");
+        scanf("%s",&usuario);
+        printf("Digite sua senha: ");
+        GetPassword(senha);
+        usuarioDigitado = strcmp(usuario,"usuarioAqui");
+        senhaDigitada = strcmp(senha,"senhaAqui");
+        while(usuarioDigitado!=0){
+            printf("\n\nUsuário ou senha incorretos! Tente novamente.\n");
+            printf("Digite seu usuário: ");
+            scanf("%s",&usuario);
+            printf("Digite sua senha: ");
+            GetPassword(senha);
+            usuarioDigitado = strcmp(usuario,"usuarioAqui");
+            senhaDigitada = strcmp(senha,"senhaAqui");
+            while (senhaDigitada!=0){
+                printf("\n\nUsuário ou senha incorretos! Tente novamente.\n");
+                printf("Digite seu usuário: ");
+                scanf("%s",&usuario);
+                printf("Digite sua senha: ");
+                GetPassword(senha);
+                usuarioDigitado = strcmp(usuario,"usuarioAqui");
+                senhaDigitada = strcmp(senha,"senhaAqui");
+            };
+        };
+
+        printf("\n\nLogin aprovado! Seja bem-vindo(a).");
+        scanf("%i",&usuarioDigitado);
     }
+    int GetPassword(char *str)
+    {
+        int i = 0,end = 0;
+        char ch;
+
+        do
+        {
+            ch=_getch();
+            switch (ch)
+            {
+                case backSpace:
+                if (i > 0)
+                {
+                    i--;
+                    str--;
+                    printf("\b");
+                    printf(" ");
+                    printf("\b");
+                }
+                break;
+
+                case newLine:
+                *str = '\0';
+                end = 1;
+                break;
+
+                default:
+                *str = ch;
+                str++ ;
+                i++;
+                printf("*");
+                break;
+            };
+        }while (end==0);
+        return(1);
+    }
+
     
     // Processamento
     
